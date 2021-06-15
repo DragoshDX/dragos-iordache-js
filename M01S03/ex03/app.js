e8a135fdaa93be9d9f21294ef7bd5ae322bd500a
Dragos Iordache <dragos.i@adoreme.com>
2021-06-15 21:47:06 +0300

    9:46

---
+++ M01S03/ex03/app.js
@@ -0,0 +1,29 @@
+var person = {
+  name: 'Dragos',
+  surname: 'Iordache',
+  friends: {},
+};
+
+var larry = {
+  name: 'Larry',
+  surname: 'Larryson',
+  friends: {},
+};
+
+var carol = {
+  name: 'Carol',
+  surname: 'Carolson',
+  friends: {},
+};
+
+var steven = {
+  name: 'Steven',
+  surname: 'Stevenson',
+  friends: {},
+};
+
+var andra = {
+  name: 'Andra',
+  surname: 'Andrason',
+  friends: {},
+};
