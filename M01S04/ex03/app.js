6c9fd538fef1c5c118b34398fa9461f64ad4534f
Dragos Iordache <dragos.i@adoreme.com>
2021-06-22 21:53:58 +0300

    drop the mic

---
+++ M01S04/ex03/app.js
@@ -0,0 +1,18 @@
+var errorCode = undefined;
+
+switch (errorCode) {
+  case 100:
+    console.log('Error 100');
+
+  case 200:
+    console.log('Error 200');
+  case 300:
+    console.log('Error 300');
+  case 400:
+    console.log('Error 400');
+
+  case 500:
+    console.log('Error 500');
+  default:
+    console.log('No error');
+}
---
+++ M01S04/ex04/app.js
@@ -0,0 +1,15 @@
+var i = 0;
+
+for (i = 1; i < 100; i++) {
+  console.log(i);
+
+  if (i >= 50) {
+    break;
+  }
+}
+
+// for (i = 1; i < 100; i++) {
+//   if (i <= 50) {
+//     console.log(i);
+//   }
+// }
