5800b7764330f8a3ef40b341eb0dc62dccbe090d
Dragos Iordache <dragosh.iordache@yahoo.com>
2021-06-29 20:01:45 +0300

    8:01

---
+++ M01S04/ex07/app.js
@@ -0,0 +1,27 @@
+const friends = [
+  {
+    name: 'Dragos',
+    surname: 'Iordache',
+    friends: {},
+  },
+  {
+    name: 'Larry',
+    surname: 'Larryson',
+    friends: {},
+  },
+  {
+    name: 'Steven',
+    surname: 'Stevenson',
+    friends: {},
+  },
+  {
+    name: 'Carol',
+    surname: 'Carolson',
+    friends: {},
+  },
+  {
+    name: 'Andra',
+    surname: 'Andrason',
+    friends: {},
+  },
+];
