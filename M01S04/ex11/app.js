0588a1683c5487491c7c00d5ec00388a800854d2
Dragos Iordache <dragosh.iordache@yahoo.com>
2021-07-01 20:40:45 +0300

    8:40

--- M01S04/ex11/app.js
+++ M01S04/ex11/app.js
@@ -27,5 +27,28 @@ const person = {
   },
 };
 
-const skillKeys = Object.keys(person.skills);
+const skillsKeys = Object.keys(person.skills);
 const friendsKeys = Object.keys(person.friends);
+
+console.warn(
+  'Folosind propozitii de forma: “person.name cunoaste: html.” “person.name cunoaste: javaScript.”',
+);
+skillsKeys.forEach(function (skillKey) {
+  const personKnows = person.skills[skillKey];
+
+  if (personKnows === true) {
+    console.log(`${person.name} cunoaste: ${skillKey}.`);
+  }
+});
+
+console.warn(
+  'Prin aceeasi metoda, afiseaza o lista inversata cu numele complet inversat al prietenilor.',
+);
+friendsKeys
+  .slice() // copy
+  .reverse() // reverse the copy + loop through the copy
+  .forEach(function (friendKey) {
+    const { surname, name } = person.friends[friendKey];
+
+    console.log(`${surname} ${name}`);
+  });
