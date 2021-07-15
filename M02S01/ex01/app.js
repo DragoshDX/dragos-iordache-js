f36cbc996d4d72231fcdfce26dcdcb8d80980176
Dragos Iordache <dragosh.iordache@yahoo.com>
2021-07-15 21:50:50 +0300

    9:50

---
+++ M02S01/ex01/app.js
@@ -0,0 +1,16 @@
+class Car {
+  constructor(make, color, wheels, speed) {
+    this.make = make;
+    this.color = color;
+    this.wheels = wheels;
+    this.speed = speed;
+  }
+
+  accelerate() {
+    this.speed++;
+  }
+
+  decelerate() {
+    this.speed--;
+  }
+}
